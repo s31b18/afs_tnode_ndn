2ad7fc1966da156590d9f5f3ff7f35be6f435666
Dearkano <vaynetian@cc98.org>
2019-09-11 16:47:03 +0800

    update

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const RNStart = require('./ndn')
 const fs = require('fs')
 module.exports = app => {
-    app.beforeStart(async () => {
-        const rs = fs.readFileSync(`${__dirname}/config.json`)
-        const config = JSON.parse(rs)
-        const cluster = config.cluster
-        console.log(`==app beforeStart, start Rnode, cluster is ${cluster}==`);
-        app.cluster = cluster
-        RNStart(app)
-    });
+    // app.beforeStart(async () => {
+    //     // const rs = fs.readFileSync(`${__dirname}/config.json`)
+    //     // const config = JSON.parse(rs)
+    //     // const cluster = config.cluster
+    //     // console.log(`==app beforeStart, start Rnode, cluster is ${cluster}==`);
+    //     // app.cluster = cluster
+    //     RNStart(app)
+    // });
 
     // app.ready(async () => {
     //     setInterval(
