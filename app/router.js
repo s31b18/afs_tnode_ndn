10f7792381858eff73daa009a444e88200bd95e6
Dearkano <vaynetian@cc98.org>
2019-09-13 11:00:38 +0800

    update

--- app/router.js
+++ app/router.js
@@ -7,9 +7,4 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
   router.get('/getFileInfo', controller.publish.getFileInfo);
-  router.get('/download', controller.publish.downloadFile);
-  router.get('/downloadFile', controller.publish.downloadIFile);
-  router.get('/getDownloadInfo', controller.publish.getDownloadInfo);
-  router.post('/file_record/afid', controller.publish.getFileRecord)
-  router.get('/getFileParameter', controller.publish.getFileParameter);
 };
